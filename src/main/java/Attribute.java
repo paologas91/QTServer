81ca811be91672a2aae9c633e0229a6d30c6f931
peppocola <giuseppecolavito999@gmail.com>
2019-04-25 16:26:50 +0200

    fix visibilità e checkstile Attribute.java

--- src/main/java/Attribute.java
+++ src/main/java/Attribute.java
@@ -1,43 +1,42 @@
 
-public abstract class Attribute {
-	
+abstract class Attribute {
+
 	private String name;
 	private int index;
-	
+
 	/**
 	 * Inizializza i valori dei membri name, index
 	 * @param name nome dell'attributo
 	 * @param index identificativo numerico dell'attributo
 	 */
-	
-	protected Attribute(String name, int index) {   // la classe è astratta pertanto non può essere public
+
+	Attribute(final String name, final int index) {		// la classe è astratta pertanto non può essere public
 		
 		this.name = name;
 		this.index = index;
 	}
-	
+
 	/**
-	 * 
 	 * @return nome dell'attributo
 	 */
 	
-	public String getName() {
-		
-		return name;   // oppure this.name
+	String getName() {
+
+		return name;	// oppure this.name
 	}
 	
-	public int getIndex() {
-		
+	int getIndex() {
+
 		return index;
 	}
-	
+
 	/*
 	 * evitando di scrivere il toString(), verrà utilizzato quello di default della classe Object
 	 */
 
 	@Override
 	public String toString() { 
-		
+
 		return name;
 	}
 }
