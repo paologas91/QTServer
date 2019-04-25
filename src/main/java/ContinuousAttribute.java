8da7f7d82b15eb218bfce2cb6abf68a9cebcb0d3
peppocola <giuseppecolavito999@gmail.com>
2019-04-25 16:27:25 +0200

    fix visibilità e checkstile ContinuousAttribute.java

--- src/main/java/ContinuousAttribute.java
+++ src/main/java/ContinuousAttribute.java
@@ -1,24 +1,24 @@
 
-public class ContinuousAttribute extends Attribute {
+class ContinuousAttribute extends Attribute {
 
 	// attributi d'istanza
-	
+
 	private double max;
 	private double min;
-	
+
 	// metodi
-	
-	public ContinuousAttribute(String name, int index, double min, double max) {
-		
-		super(name, index); // chiama il costruttore della classe madre
+
+	ContinuousAttribute(final String name, final int index, final double min, final double max) {
+
+		super(name, index);	// chiama il costruttore della classe madre
 		this.max = max;
 		this.min = min;
 	}
-	
-	public double getScaledValue(double v) {
-		
+
+	double getScaledValue(final double v) {
+
 		double v1 = (v-min)/(max-min);
-				
+
 		return v1;
 	}
 }
