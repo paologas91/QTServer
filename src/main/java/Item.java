312dadad08cc69fec1af0a8d6ea009fee7b0023c
GreenSully <antoniogrisulli23@gmail.com>
2019-04-26 08:39:58 +0200

    risolti alcuni errori di checkstyle in Item.java

--- src/main/java/Item.java
+++ src/main/java/Item.java
@@ -3,27 +3,27 @@ abstract class Item {
 
 	private Attribute attribute;
 	private Object value;
-	
-	Item(Attribute attribute, Object value) {
-		
+
+	Item(final Attribute attribute, final Object value) {
+
 		this.attribute = attribute;
 		this.value = value;
 	}
-	
+
 	Attribute getAttribute() {
-		
+
 		return attribute;
 	}
-	
+
 	Object getValue() {
-		
+
 		return value;
 	}
-	
+
 	public String toString() {
-		
+
 		return value.toString();
 	}
-	
+
 	abstract double distance(Object a);
 }
