84a45880b099b777329bd09b90e49f957024abec
Paolo Gasparro <gasparro.paolo@gmail.com>
2019-05-16 10:00:50 +0200

    prime modifiche

---
+++ src/main/java/data/ContinuousItem.java
@@ -0,0 +1,15 @@
+package data;
+
+public class ContinuousItem extends Item {
+	
+	public ContinuousItem(final Attribute attribute, final double value) {
+		super(attribute, value);
+	}
+	
+	double distance(Object a) {
+		// do I need RTTI?
+		return (double) Math.abs( ((ContinuousAttribute) this.getAttribute()).getScaledValue((double) this.getValue()) - 
+				((ContinuousAttribute) ((Item) a).getAttribute()).getScaledValue((double) ((Item) a).getValue()));
+	}
+
+}
--- src/main/java/data/Data.java
+++ src/main/java/data/Data.java
@@ -14,8 +14,8 @@ public class Data {
 		//data
 
 		data = new Object[14][5];
-
-		data[0] = new String[]{"sunny", "hot", "high", "weak", "no"};
+/*
+		data[0] = new String[]{"sunny", 30.3, "high", "weak", "no"};
 		data[1] = new String[]{"sunny", "hot", "high", "strong", "no"};
 		data[2] = new String[]{"overcast", "hot", "high", "weak", "yes"};
 		data[3] = new String[]{"rain", "mild", "high", "weak", "yes"};
@@ -29,6 +29,81 @@ public class Data {
 		data[11] = new String[]{"overcast", "mild", "high", "strong", "yes"};
 		data[12] = new String[]{"overcast", "hot", "normal", "weak", "yes"};
 		data[13] = new String[]{"rain", "mild", "high", "strong", "no"};
+*/
+		data[0][0] = new String("sunny");
+		data[1][0] = new String("sunny");
+		data[2][0] = new String("overcast");
+		data[3][0] = new String("rain");
+		data[4][0] = new String("rain");
+		data[5][0] = new String("rain");
+		data[6][0] = new String("overcast");
+		data[7][0] = new String("sunny");
+		data[8][0] = new String("sunny");
+		data[9][0] = new String("rain");
+		data[10][0] = new String("sunny");
+		data[11][0] = new String("overcast");
+		data[12][0] = new String("overcast");
+		data[13][0] = new String("rain");
+
+		data[0][1] = new Double (30.3);
+		data[1][1] = new Double (30.3);
+		data[2][1] = new Double (30);
+		data[3][1] = new Double (13);
+		data[4][1] = new Double (0);
+		data[5][1] = new Double (0);
+		data[6][1] = new Double (0.1);
+		data[7][1] = new Double (13);
+		data[8][1] = new Double (0.1);
+		data[9][1] = new Double (12);
+		data[10][1] = new Double (12.5);
+		data[11][1] = new Double (12.5);
+		data[12][1] = new Double(29.21);
+		data[13][1] = new Double (12.5);
+		
+		data[0][2] = new String("high");
+		data[1][2] = new String("high");
+		data[2][2] = new String("high");
+		data[3][2] = new String("high");
+		data[4][2] = new String("normal");
+		data[5][2] = new String("normal");
+		data[6][2] = new String("normal");
+		data[7][2] = new String("high");
+		data[8][2] = new String("normal");
+		data[9][2] = new String("normal");
+		data[10][2] = new String("normal");
+		data[11][2] = new String("high");
+		data[12][2] = new String("normal");
+		data[13][2] = new String("high");
+		
+		data[0][3] = new String("weak");
+		data[1][3] = new String("strong");
+		data[2][3] = new String("weak");
+		data[3][3] = new String("weak");
+		data[4][3] = new String("weak");
+		data[5][3] = new String("strong");
+		data[6][3] = new String("strong");
+		data[7][3] = new String("weak");
+		data[8][3] = new String("weak");
+		data[9][3] = new String("weak");
+		data[10][3] = new String("strong");
+		data[11][3] = new String("strong");
+		data[12][3] = new String("weak");
+		data[13][3] = new String("strong");
+		
+		data[0][4] = new String("no");
+		data[1][4] = new String("no");
+		data[2][4] = new String("yes");
+		data[3][4] = new String("yes");
+		data[4][4] = new String("yes");
+		data[5][4] = new String("no");
+		data[6][4] = new String("yes");
+		data[7][4] = new String("no");
+		data[8][4] = new String("yes");
+		data[9][4] = new String("yes");
+		data[10][4] = new String("yes");
+		data[11][4] = new String("yes");
+		data[12][4] = new String("yes");
+		data[13][4] = new String("no");
 
 		// numberOfExamples
 
@@ -45,11 +120,7 @@ public class Data {
 		outLookValues[2] = "sunny";
 		attributeSet.add(new DiscreteAttribute("Outlook", 0, outLookValues));
 
-		String[] temperatureValues = new String[3];
-		temperatureValues[0] = "hot";
-		temperatureValues[1] = "mild";
-		temperatureValues[2] = "cool";
-		attributeSet.add(new DiscreteAttribute("Temperature", 1, temperatureValues));
+		attributeSet.add(new ContinuousAttribute("Temperature", 1, 3.2, 38.7));
 
 		String[] humidityValues = new String[2];
 		humidityValues[0] = "high";
@@ -90,7 +161,11 @@ public class Data {
 	public Tuple getItemSet(final int index) {
 		Tuple tuple = new Tuple(attributeSet.size());
 		for (int i = 0; i < attributeSet.size(); i++) {
-			tuple.add(new DiscreteItem((DiscreteAttribute) attributeSet.get(i), (String) data[index][i]), i);
+			if (attributeSet.get(i) instanceof DiscreteAttribute) {
+				tuple.add(new DiscreteItem((DiscreteAttribute) attributeSet.get(i), (String) data[index][i]), i);
+			} else {
+				tuple.add(new ContinuousItem((ContinuousAttribute) attributeSet.get(i), (double) data[index][i]), i); // dubbio
+			}
 		}
 		return tuple;
 	}
