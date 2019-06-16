b0f16f9c5362651c0351cbcf249091b24d72757a
GreenSully <antoniogrisulli23@gmail.com>
2019-06-16 15:53:28 +0200

    aggiunto test per continuousattribute

---
+++ src/test/java/data/ContinuousAttributeTest.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach; 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ContinuousAttributeTest {
+
+	@Test
+	void test() {
+		asserEqual(1,1);
+	}
+
+}
