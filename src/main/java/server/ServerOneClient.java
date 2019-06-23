a1546cc7abb1dae3c6db193c03641cd7f6f4b22e
peppocola <giuseppecolavito999@gmail.com>
2019-06-23 11:16:20 +0200

    comunicazione

--- src/main/java/server/ServerOneClient.java
+++ src/main/java/server/ServerOneClient.java
@@ -40,61 +40,61 @@ public class ServerOneClient extends Thread {
 		try {
 			while (cicle == true) {
 				System.out.println("in attesa dell'operazione");
-				int operation = (int) in.readObject();
+				final int operation = (int) in.readObject();
 				System.out.println("scelta operazione numero: " + operation);
 				switch (operation) {
-					case 0:
-						tabName = (String) in.readObject();
-						data = new Data(tabName);
-						out.writeObject("OK");
-						break;
-					case 1:
-						radius = (Double) in.readObject();
-						qt = new QTMiner(radius);
-						final int num = qt.compute(data);
-						out.writeObject("OK");
-						out.writeObject(num);
-						out.writeObject(qt.toString());
-						break;
-					case 2:
-						qt.salva(tabName + "_" + radius + ".dmp");
-						out.writeObject("OK");
-						break;
-					case 3:
-						final String file = (String) in.readObject() + "_" + (double) in.readObject()
-						+ ".dmp";
-						System.out.println(file);
-						qt = new QTMiner(file);
-						out.writeObject("OK");
-						System.out.println(qt);
-						out.writeObject(qt.toString());
-						break;
-					case 4:
-						DbAccess db = new DbAccess();
-						LinkedList<String> tables = new LinkedList<String>();
-						try {
-							db.initConnection();
-							Connection c = db.getConnection();
-							Statement s = c.createStatement();
-							ResultSet r = s.executeQuery("show tables");
-							while (r.next()) {
-								tables.add(r.getString(1));
-							}
-							out.writeObject(tables);
-							s.close();
-						} catch (DatabaseConnectionException e) {
-							e.printStackTrace();
-						} catch (SQLException e) {
-							e.printStackTrace();
-						} finally {
-							db.closeConnection();
+				case 0:
+					tabName = (String) in.readObject();
+					data = new Data(tabName);
+					out.writeObject("OK");
+					break;
+				case 1:
+					radius = (Double) in.readObject();
+					qt = new QTMiner(radius);
+					final int num = qt.compute(data);
+					out.writeObject("OK");
+					out.writeObject(num);
+					out.writeObject(qt.toString());
+					break;
+				case 2:
+					qt.salva(tabName + "_" + radius + ".dmp");
+					out.writeObject("OK");
+					break;
+				case 3:
+					final String file = (String) in.readObject() + "_" + (double) in.readObject()
+							+ ".dmp";
+					System.out.println(file);
+					qt = new QTMiner(file);
+					out.writeObject("OK");
+					System.out.println(qt);
+					out.writeObject(qt.toString());
+					break;
+				case 4:
+					final DbAccess db = new DbAccess();
+					final LinkedList<String> tables = new LinkedList<String>();
+					try {
+						db.initConnection();
+						final Connection c = db.getConnection();
+						final Statement s = c.createStatement();
+						final ResultSet r = s.executeQuery("show tables");
+						while (r.next()) {
+							tables.add(r.getString(1));
 						}
-						break;
-					case 5: 
-						cicle = false;
-						break;
-					default:
-						break;
+						out.writeObject(tables);
+						s.close();
+					} catch (final DatabaseConnectionException e) {
+						e.printStackTrace();
+					} catch (final SQLException e) {
+						e.printStackTrace();
+					} finally {
+						db.closeConnection();
+					}
+					break;
+				case 5:
+					cicle = false;
+					break;
+				default:
+					break;
 				}
 			}
 		} catch (final IOException e) {
@@ -102,9 +102,24 @@ public class ServerOneClient extends Thread {
 		} catch (final ClassNotFoundException e) {
 			e.printStackTrace();
 		} catch (final ClusteringRadiusException e) {
+
+			try {
+				out.writeObject("full");
+			} catch (final IOException e1) {
+				e1.printStackTrace();
+			}
+
 			e.printStackTrace();
+
 		} catch (final EmptyDatasetException e) {
+
+			try {
+				out.writeObject("empty");
+			} catch (final IOException e1) {
+				e1.printStackTrace();
+			}
 			e.printStackTrace();
+
 		} finally {
 			try {
 				socket.close();
